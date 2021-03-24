4a33a4a87478287a46648bd4d6a84012c3dbaf16
nicolas-martin <nicolas.martin.0986@gmail.com>
2021-03-24 13:10:35 -0400

    tidy log results

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+
+	"github.com/nicolas-martin/log-benchmark/hooks"
+	"github.com/nicolas-martin/log-benchmark/regex"
+	"github.com/sirupsen/logrus"
+)
 
 func main() {
-	fmt.Println("a")
+	resHookInfo := testing.Benchmark(BenchmarkHooksInfo)
+	resHookErr := testing.Benchmark(BenchmarkHooksErr)
+	resRegexInfo := testing.Benchmark(BenchmarkRegexInfo)
+	resRegexErr := testing.Benchmark(BenchmarkRegexErr)
+
+	fmt.Printf("HookInfo\t %s\n", resHookInfo)
+	fmt.Printf("HookErr\t\t %s\n", resHookErr)
+	fmt.Printf("RegexInfo\t %s\n", resRegexInfo)
+	fmt.Printf("RegexErr\t %s\n", resRegexErr)
+}
+
+func benchmarkLog(l *logrus.Logger, level logrus.Level, b *testing.B, msg string) {
+	for n := 0; n < b.N; n++ {
+		l.Log(level, msg)
+	}
+}
+
+func BenchmarkHooksInfo(b *testing.B) {
+	l := hooks.Setup()
+	benchmarkLog(l, logrus.InfoLevel, b, "Info Hook")
+}
+
+func BenchmarkHooksErr(b *testing.B) {
+	l := hooks.Setup()
+	benchmarkLog(l, logrus.ErrorLevel, b, "Err Hook")
+}
+
+func BenchmarkRegexInfo(b *testing.B) {
+	l := regex.Setup()
+	benchmarkLog(l, logrus.InfoLevel, b, "Info Regex")
+}
+
+func BenchmarkRegexErr(b *testing.B) {
+	l := regex.Setup()
+	benchmarkLog(l, logrus.ErrorLevel, b, "Err Regex")
 }
---
+++ readme.md
@@ -0,0 +1,8 @@
+** Results **
+
+|Test|||
+|---|---|---|
+|HookInfo            |31689            |33846 ns/op|
+|HookErr             |29824            |40997 ns/op|
+|RegexInfo           |44926            |28788 ns/op|
+|RegexErr            |46003            |27248 ns/op|
